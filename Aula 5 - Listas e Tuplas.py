df1b6cb03977f5ea34a4d359f8aece9a440fb10a
Jhonnylibra <83561438+Jhonnylibra@users.noreply.github.com>
2021-05-16 21:50:19 -0300

    Add files via upload

---
+++ Aula 5 - Listas e Tuplas.py
@@ -0,0 +1,30 @@
+lista = [10, 23, 85, 7]
+lista_animal = ['Cachorro', 'Gato', 'Elefante', 'Arara', 'Papagaio']
+
+# BUSCAR DENTRO DA LISTA E ADICIONAR CASO NÃO EXISTA ATRAVÉS DO APPEND.
+
+# nomeanimal = str(input('Digite o nome do animal desejado: '))
+# if nomeanimal in lista_animal:
+#     print('O animal já está cadastrado na lista.')
+#     lista_animal.sort()
+#     print(lista_animal)
+# else:
+#     print('Animal não cadastrado na lista. Será adicionado neste instante...')
+#     lista_animal.append(nomeanimal.format())
+#     lista_animal.sort()
+#     print (lista_animal)
+
+# print(lista)
+# print(lista_animal)
+
+# RETORNAR MAIOR VALOR DA LISTA
+# print(max(lista))
+
+# RETORNAR MENOR VALOR DA LISTA
+# print(min(lista))
+
+# ORDENAR AS LISTAS
+# lista.sort()
+# lista_animal.sort()
+# print(lista)
+# print(lista_animal)
